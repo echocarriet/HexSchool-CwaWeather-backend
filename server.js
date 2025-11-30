535717e51bdcf23138e84fdb29d1cf6df3bb4b80
carrieTseng <echocarriet@gmail.com>
2025-11-30 17:29:08 +0800

    調整縣市

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const getKaohsiungWeather = async (req, res) => {
       {
         params: {
           Authorization: CWA_API_KEY,
-          locationName: "高雄市",
+          locationName: "台南市",
         },
       }
     );
@@ -48,7 +48,7 @@ const getKaohsiungWeather = async (req, res) => {
     if (!locationData) {
       return res.status(404).json({
         error: "查無資料",
-        message: "無法取得高雄市天氣資料",
+        message: "無法取得台南市天氣資料",
       });
     }
 
